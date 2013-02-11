f7b83f68e7daf5c2411406d367006423f3d3adf1
Daniel Rodríguez <sadasant@gmail.com>
2013-02-11 12:15:18 -0430

    now with real bytes

--- go/src/crypto_005/otp.go
+++ go/src/crypto_005/otp.go
@@ -1,27 +1,13 @@
 package crypto_005
 
-import "strconv"
-
 type otp cypher
 
-func (o otp) Enc(k string, m string) string {
-	// Note: this is not really efficient,
-	// later on I'll make a way to parse
-	// raw binary data.
-	ik, _ := strconv.ParseInt(k, 2, 64)
-	im, _ := strconv.ParseInt(m, 2, 64)
-	c := strconv.FormatInt(ik^im, 2)
-	return c
+func (o otp) Enc(k byte, m byte) byte {
+	return k ^ m
 }
 
-func (c otp) Dec(k string, e string) string {
-	// Note: this is not really efficient,
-	// later on I'll make a way to parse
-	// raw binary data.
-	ik, _ := strconv.ParseInt(k, 2, 64)
-	ie, _ := strconv.ParseInt(e, 2, 64)
-	m := strconv.FormatInt(ik^ie, 2)
-	return m
+func (c otp) Dec(k byte, e byte) byte {
+	return k ^ e
 }
 
 var OTP otp
--- go/src/crypto_005/otp_test.go
+++ go/src/crypto_005/otp_test.go
@@ -11,19 +11,19 @@ func TestOTP(t *testing.T) {
 	// once is decrypted remains consistent with the original message.
 	m := "0110111"
 	k := "1011010"
-	e := OTP.Enc(k, m)
-	d := OTP.Dec(k, e)
 	im, _ := strconv.ParseInt(m, 2, 64)
-	id, _ := strconv.ParseInt(d, 2, 64)
-	if im != id {
+	ik, _ := strconv.ParseInt(k, 2, 64)
+	bm := byte(im)
+	bk := byte(ik)
+	be := OTP.Enc(bk, bm)
+	bd := OTP.Dec(bk, be)
+	if bm != bd {
 		t.Errorf("Original and decrypted messages are different.")
 		return
 	}
 	// Second, we test if only with the original message
 	// and the encrypted message we can get the key.
-	ie, _ := strconv.ParseInt(e, 2, 64)
-	ik, _ := strconv.ParseInt(k, 2, 64)
-	if im ^ ie != ik {
+	if bm ^ be != bk {
 		t.Errorf("It should be possible to compute the key from the original message and the encrypted message.")
 		return
 	}
