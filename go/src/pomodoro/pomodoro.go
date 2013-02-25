3f8d1a97c33dfb808fcfe0aafe1c333a3a407e3c
Daniel Rodríguez <sadasant@gmail.com>
2013-02-25 15:40:49 -0430

    time mistakes and captuing the whole line

--- go/src/pomodoro/pomodoro.go
+++ go/src/pomodoro/pomodoro.go
@@ -5,18 +5,19 @@ package main
 // -   [Wikipedia: Pomodoro Technique](https://en.wikipedia.org/wiki/Pomodoro_Technique)
 // -   [List of colors for prompt](https://wiki.archlinux.org/index.php/Color_Bash_Prompt#List_of_colors_for_prompt_and_Bash)
 // -   [CMD in Python and Go](http://www.darkcoding.net/software/pretty-command-line-console-output-on-unix-in-python-and-go-lang/)
-// -   [fmt.Scanf](http://golang.org/pkg/fmt/#Scanf)
 // -   [time.Tick](http://golang.org/pkg/time/#example_Tick)
 // -   [strconv.Itoa](http://golang.org/pkg/strconv/#Itoa)
+// -   [Capture spaced user input](http://stackoverflow.com/questions/7452641/capture-spaced-user-input)
 //
 // It'd be cool to make it with ncurses:
 // -   [ncurses with go](https://code.google.com/p/goncurses/source/browse/ncurses.go)
 //
 
 import (
-	"fmt"
 	"strconv"
 	"time"
+	"os"
+	"bufio"
 )
 
 func wait(start string, minutes int, end string) {
@@ -34,15 +35,17 @@ func wait(start string, minutes int, end string) {
 		}
 		print("\r"+start, smins+":"+ssecs, end)
 		if seconds == 0 {
+			if minutes == 0 {
+				break
+			}
 			minutes--
 			seconds = 60
-		}
-		if minutes == 0 {
-			break
 		}
 		seconds--
 	}
 }
+
+const delim = '\n'
 
 func main() {
 	var task_name string
@@ -57,18 +60,21 @@ func main() {
 	green := "\033[0;32m"
 	end := "\033[0m"
 	task_count := 1
+	r := bufio.NewReader(os.Stdin)
 	for {
 		println("\n"+bwhite+"Pomodoro! "+white+"Nº", task_count, end)
 		print("Name your task: ")
-		fmt.Scanln(&task_name)
+		task_name, _ = r.ReadString(delim)
+		task_name = task_name[:len(task_name)-1]
 		wait(red+"Waiting ", 25, " minutes for “"+bred+task_name+red+"” :|"+end)
-		fmt.Print("\x07") // ASCII code 7 (BEL), or "\a"
+		print("\x07") // ASCII code 7 (BEL), or "\a"
 		print("\r" + green + "Finished “" + bgreen + task_name + green + "” :)                 \n" + end)
 		switch task_count % 4 {
 		case 0:
 		ask_break:
 			print("Short (s) or long (l) break?")
-			fmt.Scanln(&which_break)
+			which_break, _ = r.ReadString(delim)
+			which_break = which_break[:len(which_break)-1]
 			switch which_break {
 			case "s":
 				break_time = 15
