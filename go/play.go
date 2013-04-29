f56da1d0c674bde23cd27fa8da2926b1a7c7a247
Daniel Rodriguez <sadasant@gmail.com>
2013-04-29 20:34:37 +0000

    play.go

---
+++ go/play.go
@@ -0,0 +1,17 @@
+// play.go
+// Just like http://play.golang.org/
+// To run it within vim, do:
+//
+//     :bel vnew  | r!go run %
+//
+// Remember to save as it was originally.
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, playground")
+}
