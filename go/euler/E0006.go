35e667c17cbd4a42f1afaf0dd2cc3e38eb969eac
Daniel Rodrí­guez <sadasant@gmail.com>
2014-02-09 15:39:51 -0430

    Using our Euler package in problems 6 and 7, removed the benchmark

--- go/euler/E0006.go
+++ go/euler/E0006.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	. "fmt"
-	. "math"
+	"github.com/sadasant/scripts/go/euler/euler"
+	"math"
 )
 
-func main() {
+func solution(v ...int) interface{} {
 	var n, sum_sq, sq_sum float64
-
-	Print("The difference between the sum of the squares and the square of the sum of the natural numbers up to ")
-	Scan(&n)
-
+	n = float64(v[0])
 	for i := 1.0; i <= n; i++ {
-		sum_sq += Pow(i, 2.0)
+		sum_sq += math.Pow(i, 2.0)
 		sq_sum += i
 	}
-	sq_sum = Pow(sq_sum, 2.0)
+	return math.Pow(sq_sum, 2.0) - sum_sq
+}
 
-	Println("is", int(sq_sum - sum_sq))
+func main() {
+	euler.Init(6, "Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.")
+	euler.PrintTime("Result: %v, Nanoseconds: %d\n", solution, 100)
 }
--- go/euler/E0007.go
+++ go/euler/E0007.go
@@ -1,23 +1,42 @@
 package main
 
 import (
-	. "fmt"
-	. "math"
+	"github.com/sadasant/scripts/go/euler/euler"
+	"math"
 )
 
-func PrimeAt(n int) int {
-	if n < 3 {
-		return n+1
+func AppendingPrimes(v ...int) interface{} {
+	n := v[0]
+	primes := []int{3,5,7,11,13}
+	c := len(primes)
+	i := primes[c-1]+2
+L:
+	for ; c < n; i+=2 {
+		for _, v := range primes {
+			if i%v == 0 {
+				continue L
+			}
+		}
+		primes = append(primes, i)
+		c++
 	}
-	nums := make([]bool, 3+n*int(Sqrt(float64(n))))
+	return primes[n-2]
+}
+
+func Boolean(v ...int) interface{} {
+	n := v[0]
+	if n < 3 {
+		return n + 1
+	}
+	nums := make([]bool, 3+n*int(math.Sqrt(float64(n))))
 	c := 1
-	for i, l := 3, len(nums); i < l; i+=2 {
+	for i, l := 3, len(nums); i < l; i += 2 {
 		if !nums[i] {
 			c++
 			if c == n {
 				return i
 			}
-			for j := i; j < l; j+=i {
+			for j := i; j < l; j += i {
 				nums[j] = true
 			}
 		}
@@ -25,11 +44,46 @@ func PrimeAt(n int) int {
 	return 0
 }
 
+func Overview(v ...int) interface{} {
+	isPrime := func(n int) bool {
+		switch {
+		case n==1:
+			return false
+		case n<4:
+			return true
+		case n%2 == 0:
+			return false
+		case n<9:
+			return true
+		case n%3 == 0:
+			return false
+		}
+		r := int(math.Floor(math.Sqrt(float64(n))))
+		f := 5
+		for f <= r {
+			if n%f == 0 || n%(f+2) == 0 {
+				return false
+			}
+			f += 6
+		}
+		return true
+	}
+	c := 3
+	for count := 1; count < v[0]; c += 2 {
+		if isPrime(c) {
+			count++
+		}
+	}
+	return c-2
+}
+ 
+
 func main() {
-	var n int
-
-	Print("What is the prime number in position ")
-	Scan(&n)
-
-	Println("is", PrimeAt(n))
+	euler.Init(7, "What is the Nst prime number?")
+	euler.PrintTime("N = 6     | Appending primes.       Result: %v,\t\tNanoseconds: %d\n", AppendingPrimes, 6)
+	euler.PrintTime("N = 6     | With boolean array.     Result: %v,\t\tNanoseconds: %d\n", Boolean, 6)
+	euler.PrintTime("N = 6     | With the overview code. Result: %v,\t\tNanoseconds: %d\n", Overview, 6)
+	euler.PrintTime("N = 10001 | Appending primes.       Result: %v,\tNanoseconds: %d\n", AppendingPrimes, 10001)
+	euler.PrintTime("N = 10001 | With boolean array.     Result: %v,\tNanoseconds: %d\n", Boolean, 10001)
+	euler.PrintTime("N = 10001 | With the overview code. Result: %v,\tNanoseconds: %d\n", Overview, 10001)
 }
