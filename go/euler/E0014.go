5dd086735ce1be19ed6a820fdecb76e510d91f85
Daniel Rodrí­guez <sadasant@gmail.com>
2014-02-12 02:14:32 -0430

    Updated Euler's 14 to use our Euler package

--- go/euler/E0014.go
+++ go/euler/E0014.go
@@ -1,36 +1,38 @@
-// Euler 14 http://projecteuler.net/problem=14
-// by Daniel Rodríguez http://sadasant.com
-
 package main
 
-import "fmt"
+import "github.com/sadasant/scripts/go/euler/euler"
 
-func main() {
-	var longest, chain int
-	var longest_n int64
-	var n int64 = 1
-	for ; n < 1000000; n++ {
+func Collatz(n int) int {
+	var chain int
+	for {
+		chain++
+		switch {
+		case n == 1:
+			return chain
+		case n%2 == 0:
+			n = n / 2
+		default:
+			n = 3*n + 1
+		}
+	}
+}
+
+func solution(v ...int) interface{} {
+	var longest, longest_n, chain int
+	for n := 1; n < v[0]; n++ {
 		chain = Collatz(n)
 		if chain > longest {
 			longest = chain
 			longest_n = n
 		}
 	}
-	fmt.Println(longest_n)
-	return
+	return longest_n
 }
 
-func Collatz(n int64) int {
-	var chain int
-L:
-	chain++
-	switch {
-	case n == 1:
-		return chain
-	case n%2 == 0:
-		n = n / 2
-	default:
-		n = 3*n + 1
-	}
-	goto L
+func main() {
+	euler.Init(14, "Using the rules:\n\n"+
+		"  n -> n/2 (n is even)\n"+
+		"  n -> 3n + 1 (n is odd)\n\n"+
+		"Which starting number, under one million, produces the longest chain?")
+	euler.PrintTime("Result: %v, Nanoseconds: %d\n", solution, 1e6)
 }
