3e44eff23764428fcdbebb087a7e0a145fd84dc9
Daniel Rodrí­guez <sadasant@gmail.com>
2014-03-03 19:13:39 -0430

    js script to extract facebook comments in markdown format

---
+++ js/etc/FacebookCommentsToMarkdown.js
@@ -0,0 +1,19 @@
+// Facebook Comments to Markdown.
+// ===
+// _By Daniel Rodríguez (@sadasant)_  
+// To run it, copy the following code in the javascript console of the conversation's page.
+// To go to the conversation's page, click on the little gray date below the original post.
+Array.prototype.map.call(document.getElementsByClassName("UFICommentActorName"), function(e) {
+    var date = e.parentNode.nextSibling.firstChild.firstChild.firstChild.innerHTML;
+    var name = e.text;
+    var content = e.nextSibling.nextSibling.firstChild.innerHTML.replace(/<[^>]*>/g,"");
+    var links = e.nextSibling.nextSibling.firstChild.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        content = content.replace(/https{0,1}:\/\/[^\s]*.../, "<"+links[i].attributes.href.value+">");
+    }
+    return [
+        "_"+date+"_  ",
+        "**"+name+":**  ",
+        content
+    ].join("\n");
+}).join("\n\n---\n\n");
