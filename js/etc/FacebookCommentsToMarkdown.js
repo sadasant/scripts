e2611595a190b63610ca003597d945efb6e68686
Daniel Rodrí­guez <sadasant@gmail.com>
2014-03-03 19:46:11 -0430

    A reminder, also remove false positives, and better dates.

--- js/etc/FacebookCommentsToMarkdown.js
+++ js/etc/FacebookCommentsToMarkdown.js
@@ -3,8 +3,11 @@
 // _By Daniel Rodríguez (@sadasant)_  
 // To run it, copy the following code in the javascript console of the conversation's page.
 // To go to the conversation's page, click on the little gray date below the original post.
-Array.prototype.map.call(document.getElementsByClassName("UFICommentActorName"), function(e) {
-    var date = e.parentNode.nextSibling.firstChild.firstChild.firstChild.innerHTML;
+// Remember to load all the previous comments (they appear hidden if there are too many).
+Array.prototype.filter.call(document.getElementsByClassName("UFICommentActorName"), function(e) {
+    return e.parentNode.nextSibling.firstChild.firstChild.firstChild;
+}).map(function(e) {
+    var date = e.parentNode.nextSibling.firstChild.firstChild.firstChild.attributes.title.value;
     var name = e.text;
     var content = e.nextSibling.nextSibling.firstChild.innerHTML.replace(/<[^>]*>/g,"");
     var links = e.nextSibling.nextSibling.firstChild.getElementsByTagName("a");
