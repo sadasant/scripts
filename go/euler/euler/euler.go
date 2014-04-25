778ae7b5ba99db9e8865c923ab8eb81465075e6d
Daniel Rodrí­guez <sadasant@gmail.com>
2014-04-25 14:43:43 -0430

    Small fix for the Init printf

--- go/euler/euler/euler.go
+++ go/euler/euler/euler.go
@@ -34,7 +34,7 @@ const (
 )
 
 func Init(n int, text string) {
-	fmt.Printf("%sProject Euler, problem %s%s%s\n%s%s\n", b_yellow, n, normal, yellow, text, normal)
+	fmt.Printf("%sProject Euler, problem %d%s%s\n%s%s\n", b_yellow, n, normal, yellow, text, normal)
 }
 
 func Time(f interface{}, v ...interface{}) (interface{}, time.Duration) {
