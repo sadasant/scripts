a72c293ec20423fca8085f28c10b6d3f1f12f1ed
Daniel Rodríguez <sadasant@gmail.com>
2019-07-09 20:01:52 +0000

    RS512 passing

--- js/crypto/RS512.spec.ts
+++ js/crypto/RS512.spec.ts
@@ -1,50 +1,37 @@
 // RS512
-// RSASSA-PKCS1-v1_5 using SHA-512, as described in https://tools.ietf.org/html/rfc7518
+// Digital Signature with RSASSA-PKCS1-v1_5 using SHA-512
+// As described in https://tools.ietf.org/html/rfc7518
 
-import * as crypto from "crypto";
 import * as assert from "assert";
-
-async function process(processer: crypto.Cipher | crypto.Decipher, value: string): Promise<string> {
-  let result = '';
-  processer.on('readable', () => {
-    let chunk;
-    while (null !== (chunk = processer.read())) {
-      result += chunk.toString('hex');
-    }
-  });
-  processer.write(value);
-  processer.end();
-  return result;
-}
+import * as crypto from "crypto";
+import fs from "fs";
 
 function getTitles(that: any): string {
   return `${that!.title}-${that!.test!.title}`;
 }
  
 describe(`RS512
-  RSASSA-PKCS1-v1_5 using SHA-512, as described in https://tools.ietf.org/html/rfc7518`, () => {
+  Digital Signature with RSASSA-PKCS1-v1_5 using SHA-512`, () => {
 
-  const algorithm = "rsa_pkcs1_sha256";
-  const password = "ornithorhynchus anatinus"
+  // A key of size 2048 bits or larger MUST be used with these algorithms.
+  // Made with: openssl req -new -newkey rsa:2048 -sha512 -nodes -keyout RS512.pkey -subj "/C=US/ST=FL/L=DORAL/O=MICROSOFT/CN=AZURESDKTESTS" 
+  const private_key = fs.readFileSync('RS512.pkey', 'utf-8');
 
-  it("encrypt and decrypt", async function(): Promise<void> {
+  // Made with: openssl rsa -in RS512.pkey -pubout > RS512.pub
+  const public_key = fs.readFileSync('RS512.pub', 'utf-8')
+
+  it("sign and verify", async function(): Promise<void> {
+    // For RSA keys, the default algorithm is RSASSA-PKCS1-v1_5
+    const sign = crypto.createSign('sha512');
     const value = getTitles(this);
-    // A key of size 2048 bits or larger MUST be used with these algorithms.
-    const key = crypto.scryptSync(password, 'salt', 24);
-    // Use of an Initialization Vector (IV) of size 96 bits is REQUIRED with this algorithm.
-    const iv = crypto.randomBytes(96);
-    const cipher: crypto.Cipher = crypto.createCipheriv(algorithm, key, iv);
-    const encrypted: string = await process(cipher, value);
-    const decypher: crypto.Decipher = crypto.createDecipheriv(algorithm, key, iv);
-    const decrypted: string = await process(decypher, encrypted);
-    assert.equal(decrypted, value, "Unexpected decrypted value");
+    sign.write(value);
+    sign.end();
+    const signature = sign.sign(private_key);
+
+    const verifier = crypto.createVerify('sha512');
+    verifier.update(value);
+    verifier.end();
+    assert.ok(verifier.verify(public_key, signature));
   });
-  it("sign", async function(): Promise<void> {
-  });
-  it("verify", async function(): Promise<void> {
-  });
-  it("wrap", async function(): Promise<void> {
-  });
-  it("unwrap", async function(): Promise<void> {
-  });
+
 });
